fbf5146c2d3673e4bdcf80cbf69a85420c6fe246
Jaesung <bbbjason@naver.com>
2022-10-29 12:33:36 +0700

    Programmers Study

---
+++ main.js
@@ -0,0 +1,26 @@
+
+let people=4;
+
+function solution(people){
+
+    let i=1;
+    let result=0;
+    let pizzaNum;
+    while(true){
+        let eatSame = people*i;
+        if( eatSame % 6 ==0){
+            result =eatSame;
+            return pizzaNum = result/6;
+
+        }
+        i++;
+    }
+     
+    
+   
+   
+};
+
+console.log((solution(people)) );
+
+ 
