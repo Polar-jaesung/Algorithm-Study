d358515cf5699ccf7f4cdb46c709b62e28ba3063
Jaesung <bbbjason@naver.com>
2022-10-29 15:11:11 +0700

    Programmers JS

--- Pizza_together(3)/main.js
+++ Pizza_together(3)/main.js
@@ -23,10 +23,27 @@ function solution(numbers) {
     for(let i=0; i<numbers.length; i++){
         
         sum+=numbers[i];        
-        
+    }
+    return sum/numbers.length;
+}
+
+
+// 옷가게 할인 받기 
+function solution(price) {
+    
+    
+    if(price>=100000 && price <300000){
+        return Math.floor(price*95/100)
     }
     
-    return sum/numbers.length;
+    else if(price >=300000 && price<500000){
+        return Math.floor(price*90/100)
+    }
     
-   
+    else if(price >=500000){
+         return Math.floor(price*0.8)
+    }
+    else{
+        return price
+    }
 }
