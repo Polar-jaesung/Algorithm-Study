973e631f684b89eeb54c034a4c0c39470513e4d3
Jaesung <bbbjason@naver.com>
2022-11-08 19:24:48 +0700

    Used If sentence

--- DeleteLetter/main.js
+++ DeleteLetter/main.js
@@ -19,3 +19,22 @@ function solution (a,word){
 }
 
 console.log(solution(a));
+
+// 각도기
+function solution(angle) {
+    var answer = 0;
+    
+    if (angle === 180){
+        return 4
+    }
+    else if ( angle <180 && angle >90){
+        return 3
+    }
+    
+    else if ( angle ===90){
+        return 2
+    }
+    else if ( angle <90){
+        return 1
+    }
+}
