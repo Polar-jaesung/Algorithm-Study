5c6270520e4f24a8c36658baa3bb531fc3731a1b
JaeSung Byun(Mac) <bbbjason@naver.com>
2022-11-21 14:59:11 +0700

    Used Ternary Operator

--- rspGame/main.js
+++ rspGame/main.js
@@ -15,5 +15,26 @@ function rspGame(rsp) {
     // 가위: 2 바위: 0 보: 5 
 }
 
-// 2. 구슬을 나누는 경우의 수
+// 2. 구슬을 나누는 경우의 수(해결 못함)
 
+function sharingBall(balls, share) {
+    var answer = 0;
+    return answer;
+}
+
+// 3. 점의 위치 구하기
+
+
+let dot=[-1,4]
+function findDot(dot) {
+    let answer =0;
+    for(let i=0; i<dot.length; i++){
+        answer = dot[0]>0 && dot[1]>0 ? 1
+        : dot[0] > 0 && dot[1] <0 ? 4
+        : dot[0] < 0 && dot[1] >0 ? 2
+        : 3
+    }
+    return answer;
+}
+
+console.log(findDot(dot));
